c6ba26a3ff7cfd7d6757894ca5414642281a9adb
Kunal Das <kunaldas989@gmail.com>
2022-11-03 19:04:54 +0530

    cors changes

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
   const newsApi = async()=>{
     try{
       // const proxyUrl = "https://cors-anywhere.herokuapp.com/"
-      // const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-      const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${apikey}&category=${category}&pageSize=${loadMore}`)
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+      const news = await axios.get(`https://${proxyUrl}newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${apikey}&category=${category}&pageSize=${loadMore}`)
      setNewsArray(news.data.articles);
      setNewsResult(news.data.totalResults)
     }catch(error){
